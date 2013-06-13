640756a1f7669eadd56a434481f7c243908430cd
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-13 14:05:11 -0500

    Fixed sessions bug and can add a new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,26 @@
 class UsersController < ApplicationController
 
+  skip_before_filter :require_login, :only => [:new, :create]
+  
   def admin
   end
 
   def profile
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      session[:current_user_id] = @user.id
+      flash[:success] = "You have signed up successfully!"
+      redirect_to admin_path(@user.url)
+    else
+      flash[:error] = @user.errors.full_messages.first
+      redirect_to :back
+    end
+  end
 end
