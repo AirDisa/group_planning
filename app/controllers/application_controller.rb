6bb208fc77f9d9ea4c63cd211528d79b7d7e3b11
Andrew Stamm <andrewstamm@me.com>
2013-06-13 09:49:36 -0500

    basic controller files set up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
+
+  include ApplicationHelper
+
   protect_from_forgery
+  before_filter :require_login
+
+  private
+
+  def require_login
+    unless logged_in?
+      redirect_to root_path
+    end
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
 end
