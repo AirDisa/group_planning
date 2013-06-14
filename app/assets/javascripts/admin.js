ef09bf76a625a6da48fbe2e09cdf1800f147f32b
Andrew Stamm <andrewstamm@me.com>
2013-06-13 19:01:34 -0500

    admin page slider and jquery

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,7 +1,6 @@
 $(function() {
   $('.user_admin_nav a').bind('click', function(event){
     var $anchor = $(this);
-    debugger
 
     $('html, body').stop().animate({
       scrollTop: $($anchor.attr('href')).offset().top
