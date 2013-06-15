71b80c060d73a30d75c106ce34e6df90794bbbd1
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-15 15:55:24 -0500

    fix greedy button click handler

--- app/assets/javascripts/condition.js
+++ app/assets/javascripts/condition.js
@@ -1,25 +1,3 @@
-$(document).ready(function() {
-
-//describes the conditions and makes them draggable.
-
-$('.draggable-options li').draggable({
-  helper: "clone",
-  appendTo: "body"
-});
-
-$('.droppable-options').droppable({
-  drop: function(event, ui) {
-    if (ui.draggable.attr('id') === 'min_num') {
-      $(this).before( $('#minNumSelection').html() );
-      $('#min_num').remove();
-      addPunctuation($('#min_people'));
-    } else if (ui.draggable.attr('id') === 'person') {
-      $(this).before( $('#personSelection').html() );
-      addPunctuation($('#person_selector'));
-    };
-  }
-});
-
 addPunctuation = function(input){
   var sentence_length = $('#sentence').children("span").length;
   if (sentence_length <= 4) {
@@ -28,42 +6,60 @@ addPunctuation = function(input){
     $('#sentence').append('.');
     $('.droppable-options').remove();
   }
-}
+};
 
+$(document).ready(function() {
 
-//Transition and posting between yes, yes-if, no.
-
-$('body').on('click', 'button', function(){
-  var screen = $(this).attr('id')
-  var id = $(this).data('invitee_id');
-  if (screen === 'Yes') {
-    $.post('/invitees/'+id, {status: 'Yes'} , function(){
-      $('.conditional').hide();
-      $('.yes').animate({ width: 'show' });
-    });
-  } else if (screen ==='yes_if') {
-    $('.conditional').hide();
-    $('.yes_if').animate({ width: 'show' });
-  } else if (screen === 'No') {
-    $.post('/invitees/'+id, {status: 'No'} , function(){
-      $('.conditional').hide();
-      $('.no').animate({ width: 'show' });
-    });
-  }
-});
-
-// Handling the commit on yes-if.
-$('body').on('click', '#condition_button', function(e){
-  e.preventDefault();
-  var data = $('form').serialize();
-  $.post('/conditions', data, function(){
-    $('.yes_if').hide();
-    $('.thanks').animate({ width: 'show' });
+  $('.draggable-options li').draggable({
+    helper: "clone",
+    appendTo: "body"
   });
-});
 
-//closes message window
-$('body').on('click', '#close_window', function(){
-  $(this).parent().fadeOut();
-});
+  $('.droppable-options').droppable({
+    drop: function(event, ui) {
+      if (ui.draggable.attr('id') === 'min_num') {
+        $(this).before( $('#minNumSelection').html() );
+        $('#min_num').remove();
+        addPunctuation($('#min_people'));
+      } else if (ui.draggable.attr('id') === 'person') {
+        $(this).before( $('#personSelection').html() );
+        addPunctuation($('#person_selector'));
+      };
+    }
+  });
+
+  //Transition and posting between yes, yes-if, no.
+  $('body').on('click', '.cond_buttons button', function(){
+    var screen = $(this).attr('id')
+    var id = $(this).data('invitee_id');
+    if (screen === 'Yes') {
+      $.post('/invitees/'+id, {status: 'Yes'} , function(){
+        $('.conditional').hide();
+        $('.yes').animate({ width: 'show' });
+      });
+    } else if (screen ==='yes_if') {
+      $('.conditional').hide();
+      $('.yes_if').animate({ width: 'show' });
+    } else if (screen === 'No') {
+      $.post('/invitees/'+id, {status: 'No'} , function(){
+        $('.conditional').hide();
+        $('.no').animate({ width: 'show' });
+      });
+    }
+  });
+
+  // Handling the commit on yes-if.
+  $('body').on('click', '#condition_button', function(e){
+    e.preventDefault();
+    var data = $('form').serialize();
+    $.post('/conditions', data, function(){
+      $('.yes_if').hide();
+      $('.thanks').animate({ width: 'show' });
+    });
+  });
+
+  //closes message window
+  $('body').on('click', '#close_window', function(){
+    $(this).parent().fadeOut();
+  });
 });
