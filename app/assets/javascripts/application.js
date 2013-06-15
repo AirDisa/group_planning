6422b4bcf625a7b3ea6c0334f0b0acbbdd0e8310
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-15 10:09:38 -0500

    Datepicker plugin working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,9 +2,15 @@
 //= require jquery_ujs
 //= require jquery.ui.all
 //= require best_in_place
+//= require pickadate/picker
+//= require pickadate/picker.date
 //= require_tree .
 
 $(document).ready(function() {
   /* Activating Best In Place */
   jQuery(".best_in_place").best_in_place();
+
+  $('.datepicker').pickadate();
+
 });
+
