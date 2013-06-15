56763a442a0f0814434c3de3efde626e194105af
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-14 19:17:04 -0500

    remove the unnecessary jquery ui requires

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,7 @@
 //= require jquery
 //= require jquery_ujs
-//= require jquery.ui.all
+//= require jquery.ui.draggable
+//= require jquery.ui.droppable
 //= require best_in_place
 //= require_tree .
 
