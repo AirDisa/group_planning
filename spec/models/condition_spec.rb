ae0166d41ec2785c698ebde0ad938391ef1bd699
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-15 20:55:52 -0500

    update model tests to account for new functionality

--- spec/models/condition_spec.rb
+++ spec/models/condition_spec.rb
@@ -5,7 +5,8 @@ describe Condition do
   it { should be_instance_of(Condition) }
   it { should belong_to(:invitee) }
 
-  it { should validate_presence_of(:method) }
-  it { should validate_presence_of(:value) }
+  it { should validate_presence_of(:method)   }
+  it { should validate_presence_of(:value)    }
+  it { should allow_value(1).for(:invitee_id) }
 
 end
