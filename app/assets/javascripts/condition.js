c9d21a0ad006789cb98a531694f77615b73ce633
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-17 17:54:10 -0500

    update javascripts for new stripe buttons

--- app/assets/javascripts/condition.js
+++ app/assets/javascripts/condition.js
@@ -10,7 +10,7 @@ addPunctuation = function(input){
 $(document).ready(function() {
 
   //Transition and posting between yes, yes-if, no.
-  $('body').on('click', '.cond_buttons button', function(){
+  $('.cond_buttons button').click(function(){
     var screen = $(this).attr('id')
     var id = $(this).data('invitee_id');
     if (screen === 'Yes') {
@@ -30,9 +30,10 @@ $(document).ready(function() {
   });
 
   // Handling the commit on yes-if.
-  $('body').on('click', '#condition_button', function(e){
+  $('.wrapper').on('click', '#condition_button', function(e){
     e.preventDefault();
     var data = $('form').serialize();
+    debugger;
     $.post('/conditions', data, function(){
       $('.yes_if').hide();
       $('.thanks').animate({ width: 'show' });
@@ -40,24 +41,24 @@ $(document).ready(function() {
   });
 
   //closes message window
-  $('body').on('click', '#close_window', function(){
+  $('.wrapper').on('click', '#close_window', function(){
     $(this).parent().fadeOut();
   });
 
   // Drop down for conditions
-  $('body').on('click', '#condition_link', function(e){
+  $('.wrapper').on('click', '#condition_link', function(e){
     e.preventDefault();
     $('#condition_list').slideDown('fast');
   });
 
-  $('body').on('click', '#number_of_people', function(e){
+  $('.wrapper').on('click', '#number_of_people', function(e){
     e.preventDefault();
     $('#sentence').append($('#minNumSelection'));
     $('#number_of_people').hide();
     $('#condition_list').hide();
   });
 
-  $('body').on('click', '#certain_person_goes', function(e){
+  $('.wrapper').on('click', '#certain_person_goes', function(e){
     e.preventDefault();
     var person = $('#personSelection').clone();
     $('#sentence').append(person);
