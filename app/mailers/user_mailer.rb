2f0437a228675f8a04237ddc386c9b20c6821ad5
Mitch Lee <mitchlee11@gmail.com>
2013-06-17 19:44:08 -0500

    fix issues with email links and setup sendgrid

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,7 +4,7 @@ class UserMailer < ActionMailer::Base
   def welcome_email(user)
     @user = user
     email_with_name = "#{@user.full_name} <#{@user.email}>"
-    @url  = "http://groupact.heroku.com/"
+    @url  = "www.groupact.com"
     mail(:to => email_with_name, :subject => "Welcome to grouPACT!")
   end
 
