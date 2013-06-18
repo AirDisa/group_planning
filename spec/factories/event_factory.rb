4cfe06188b1f6ccbff29c8f32461f77979c07ea8
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-18 14:27:42 -0500

    fix rspec tests after change to event timezone

--- spec/factories/event_factory.rb
+++ spec/factories/event_factory.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :event do
     title 'Test Event'
-    commit_date DateTime.new(2020,1,1)
+    commit_date "Wed, 1 Jan 2020"
     creator_id 1
     image  "events/default.jpg"
     emails 'test@test.com'
