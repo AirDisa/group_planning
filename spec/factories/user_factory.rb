f0a4c6cb9e8d6dd2d8b31d2049b326c88240ec6f
Andrew Stamm <andrewstamm@me.com>
2013-06-19 14:34:32 -0500

    new profile page tests

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -8,4 +8,16 @@ FactoryGirl.define do
     password 'Password1'
     password_confirmation 'Password1'
   end
+
+  factory :user2, :class => User do
+    id '2'
+    first_name 'Sally'
+    last_name 'Jane'
+    url 'sally-jane'
+    email 'sjane@test.com'
+    password 'Secure1'
+    password_confirmation 'Secure1'
+    web 'www.test.com'
+    profile 'Cred fashion axe gentrify food truck'
+  end
 end
--- spec/requests/profile_spec.rb
+++ spec/requests/profile_spec.rb
@@ -17,17 +17,7 @@ describe 'Profile Page' do
 
     before do
       @user  = FactoryGirl.create(:user)
-      @event = FactoryGirl.create(:event)
-      @invite1 = Invitee.create(:user_id => @user.id, :event_id => @event.id)
-
-      @event2 = FactoryGirl.create(:event, :title => "New Event Title")
-      @invite2 = Invitee.create(:user_id => @user.id, :event_id => @event2.id)
-
-      @event3 = FactoryGirl.create(:event, :title => "New Event Title")
-      @invite3 = Invitee.create(:user_id => @user.id, :event_id => @event3.id, :status => 'Yes')
-
-      @event4 = FactoryGirl.create(:event, :title => "New Event Title")
-      @invite4 = Invitee.create(:user_id => @user.id, :event_id => @event4.id, :status => 'No')
+      @user2 = FactoryGirl.create(:user2)
 
       user_login(@user)
       visit profile_path(@user.url)
@@ -45,24 +35,59 @@ describe 'Profile Page' do
       page.should have_css('img.profile_pic')
     end
 
-    it "should display the user's created events" do
-      page.should have_content(@event.title)
+    it "should display an EDIT link if it's the current user's page" do
+      page.should have_link('Edit')
     end
 
-    it "should display the user's invited events" do
-      page.should have_content(@event2.title)
+    it "should not have an EDIT link for another user's profile page" do
+      visit profile_path(@user2.url)
+      page.should_not have_link('Edit')
     end
 
-    it "should display pending events" do
-      page.should have_content("pending:")
+    it "should not display a URL header if the user doesn't have one" do
+      page.should_not have_content('URL:')
     end
 
-    it "should display commited events" do
-      page.should have_content("going:")
+    it "should display the user's URL if they have one" do
+      visit profile_path(@user2.url)
+      page.should have_link(@user2.web)
     end
 
-    it "should display denied events" do
-      page.should have_content("not going:")
+    it "should display the user's written profile if they have one" do
+      visit profile_path(@user2.url)
+      page.should have_content(@user2.profile)
+    end
+
+    it "should display a placeholder if the is no written profile" do
+      page.should have_content('You have not yet written a profile, you may do so here')
+    end
+
+    it "should have a link to add a profile if the user doesn't have one saved" do
+      page.should have_link('here')
+    end
+
+  end
+
+  context "in a user's sidebar" do
+    include LoginHelpers
+
+    before do
+      @user  = FactoryGirl.create(:user)
+      @user2 = FactoryGirl.create(:user2)
+
+      @event = FactoryGirl.create(:event)
+      @invite1 = Invitee.create(:user_id => @user.id, :event_id => @event.id)
+
+      user_login(@user)
+      visit profile_path(@user.url)
+    end
+
+    it "should display a count of invited events" do
+      page.should have_content('Invited To: 1')
+    end
+
+    it "should display a list of attended events" do
+      page.should have_content('Attended: 0')
     end
 
   end
