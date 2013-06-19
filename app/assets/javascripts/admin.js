8cc2fa90f2b3f49bd41eb131226675aeefba542f
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-19 08:32:28 -0500

    cleanup comments

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,15 +1,35 @@
-  $(function() {
-    $('.user_admin_nav a').on('click', function(event){
-      var $anchor = $(this);
+$(function() {
+  $('.user_admin_nav a').on('click', function(event){
+    var $anchor = $(this);
 
-      $('html, body').stop().animate({
-        scrollTop: $($anchor.attr('href')).offset().top
-      }, 1000);
+    $('html, body').stop().animate({
+      scrollTop: $($anchor.attr('href')).offset().top
+    }, 1000);
 
-      event.preventDefault();
-    });
-
-    $('.datepicker').pickadate();
-
+    event.preventDefault();
   });
 
+  $('.datepicker').pickadate();
+});
+
+$(document).ready(function(){
+ $(function() {
+    $(".slider").jCarouselLite({
+      btnNext: ".next",
+      btnPrev: ".prev",
+      visible: 1
+    });
+
+    $('img.captify').captify({
+      speedOver: 'fast',
+      speedOut: 'normal',
+      hideDelay: 500,
+      animation: 'slide',
+      prefix: '',
+      opacity: '0.7',
+      className: 'caption-bottom',
+      position: 'bottom',
+      spanWidth: '100%'
+    });
+  });
+});
