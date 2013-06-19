cd694f66144d020f0acbf569829dc3c042653af0
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-18 21:46:29 -0500

    working carousel

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,6 +2,7 @@
 //= require jquery_ujs
 //= require jquery.ui.draggable
 //= require jquery.ui.droppable
+//= requre jquery.ui.all
 //= require best_in_place
 //= require pickadate/picker
 //= require pickadate/picker.date
@@ -10,5 +11,35 @@
 $(document).ready(function() {
   /* Activating Best In Place */
   jQuery(".best_in_place").best_in_place();
+  
+ $(function() {
+    $(".slider").jCarouselLite({
+      btnNext: ".next",
+      btnPrev: ".prev",
+      visible: 1
+    });
+
+    $('img.captify').captify({
+    // all of these options are... optional
+    // speed of the mouseover effect
+    speedOver: 'fast',
+    // speed of the mouseout effect
+    speedOut: 'normal',
+    // how long to delay the hiding of the caption after mouseout (ms)
+    hideDelay: 500,
+    // 'fade', 'slide', 'always-on'
+    animation: 'slide',
+    // text/html to be placed at the beginning of every caption
+    prefix: '',
+    // opacity of the caption on mouse over
+    opacity: '0.7',
+    // the name of the CSS class to apply to the caption box
+    className: 'caption-bottom',
+    // position of the caption (top or bottom)
+    position: 'bottom',
+    // caption span % of the image
+    spanWidth: '100%'
+  });
+  });
 });
 
